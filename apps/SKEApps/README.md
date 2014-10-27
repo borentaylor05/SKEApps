cbb0fe2964a1fdcfb8cdc50213f158d9d52ef613
Taylor Boren <borentaylor05@gmail.com>
2014-10-27 10:33:44 -0600

    Cleaned up w/ filter. Tried to rename knowledge-base.html, but this caused no page to be rendered.  Probably a problem with app.xml config, so weight watcher's view is called knowledge-base.html

--- apps/SKEApps/README.md
+++ apps/SKEApps/README.md
@@ -3,38 +3,4 @@ Lesson learned: Read docs before trying to hack the system.
 
 This App was created for a custom instance of Jive Software.  
 
-Some of our clients operate on the same instance, but each have their own knowledge base, and  each want a custom look and feel for that knowledge base.  To enable Jive 7 to accomplish these tasks, I created this app to do the following:
-	
-	- Allow custom html, css, js files for each client
-		- These files are included in the switch() in main.js
-		- The listed files are dynamically loaded to the DOM
-		- Loaded files are determined by the space_id, the lone param in main.render(space_id) 
-
-
-Since the Jive SDK compliles assets to one file, you cannot dynamically add them in the traditional way.  E.g. you cannot load stylesheets by appending a link element to the head for each stylesheet.  Instead you have to formulate a string to reference each stylesheet and then load them as a single source:
-
-Below are the base strings for dynamically loading .css, .js and .html files:
-	- To see how these are implemented, see main.js
-
-The base string for attaching .js files is: 
-
-//localhost:8080/gadgets/concat?container=default&gadget=http://localhost:8090/osapp/SKEApps/app.xml&debug=1&nocache=1&type=js&1=http://localhost:8090/osapp/SKEApps/javascripts/jquery.min.js&2=http://localhost:8090/osapp/SKEApps/javascripts/main.js
-
-** The example above has 2 .js files
-** localhost needs to be replaced for production
-
-The base string for .css is:
-
-//localhost:8080/gadgets/concat?container=default&gadget=http://localhost:8090/osapp/SKEApps/app.xml&debug=1&nocache=1&type=css&1=http://localhost:8090/osapp/SKEApps/stylesheets/bootstrap.min.css&2=http://localhost:8090/osapp/SKEApps/stylesheets/main.css
-
-** The example above has 2 .css files
-** localhost needs to be replaced for production
-
-These strings must be dynamically generated in order to get custom layouts for each client
-
-For 1 CSS file
-//localhost:8080/gadgets/proxy?container=default&gadget=http://localhost:8090/osapp/SKEApps/app.xml&debug=1&nocache=1&html_tag_context=link&url=http://localhost:8090/osapp/SKEApps/stylesheets/main.css
-
-For 1 HTML File:
-
-//localhost:8080/gadgets/proxy?container=default&gadget=http://localhost:8090/osapp/SKEApps/app.xml&debug=1&nocache=1&html_tag_context=div&url=http://localhost:8090/osapp/SKEApps/clients/"+htmlFile+"
+NOTE: Renaming views really screws stuff up, so try to get the name right the first time.
--- apps/SKEApps/public/javascripts/main.js
+++ apps/SKEApps/public/javascripts/main.js
@@ -2,67 +2,8 @@ var clients = {
 	weight_watchers: 2006,
 	hyundai: 2007
 }
-
 var main = {
-	render: function(space_id, callback){
-		switch(space_id){
-			case clients.weight_watchers:
-				var stylesheets = ["bootstrap.min.css", 
-									"ttbs3.css",
-									"clients/ww/custom.css",
-									"clients/ww/ww_mm.css",
-									"main.css"];
-
-				var javascripts = ["typeahead.bundle.min.js",
-									"myTypeahead.js",
-									"bootstrap.min.js",
-									"gadget-helper.js",
-									"megamenu.js",
-									"search.js",
-									"clients/ww/ww.js"
-									];
-
-				var html = "weight-watchers.html";
-			break;
-			case clients.hyundai:
-				var stylesheets = ["bootstrap.min.css", 
-									"ttbs3.css",
-									"main.css"];
-
-				var javascripts = ["typeahead.bundle.min.js",
-									"bootstrap.min.js",
-									"myTypeahead.js"];
-
-				var html = "hyundai.html";
-			break;
-		}
-		this.insert_css(stylesheets);
-		this.insert_html_js(html, javascripts, space_id);
-		callback();
-	},
-	insert_css: function(stylesheets){
-			var base = environment.current+"/gadgets/proxy?container=default&gadget=http:"+environment.node_cur+"/osapp/SKEApps/app.xml&debug=1&nocache=1&html_tag_context=link&url=http:"+environment.node_cur+"/osapp/SKEApps/stylesheets/"+stylesheets[0];
-			if(stylesheets.length > 1){
-				base = environment.current+"/gadgets/concat?container=default&gadget=http:"+environment.node_cur+"/osapp/SKEApps/app.xml&debug=1&nocache=1&type=css&"
-				for(var i = 1 ; i < stylesheets.length + 1 ; i++){
-					base += i.toString()+"=http:"+environment.node_cur+"/osapp/SKEApps/stylesheets/"+stylesheets[i-1]+"&";
-				}
-			}
-			$('<link rel="stylesheet" type="text/css" href="'+base+'" >')
-.appendTo("head");
-		gadgets.window.adjustHeight();
-	},
-	insert_html_js: function(html, javascripts, space_id){
-			$("body").load(environment.current+"/gadgets/proxy?container=default&gadget=http:"+environment.node_cur+"/osapp/SKEApps/app.xml&debug=1&nocache=1&html_tag_context=div&url=http:"+environment.node_cur+"/osapp/SKEApps/clients/"+html+"", function(res){
-				// must be done in callback so DOM has been loaded
-				insertJavascripts(javascripts, space_id); 
-				gadgets.window.adjustHeight();
-			});
-	}
-}
-
-var functions = {
-	ww: function(){
+	run: function(){
 		var tags = [ 'cancellation', 'account info', 'mobile app', 'member services' ];
 		typeahead.setup("#tags", tags);
 		// ww object in ww.js
@@ -76,25 +17,6 @@ var functions = {
 			$("#megamenu").empty();
 			megamenu.init("DOC-1011");
 		});
-	}
-}
-
-
-// must be done in callback so DOM has been loaded
-function insertJavascripts(javascripts, space_id){
-	var src = environment.current+"/gadgets/proxy?container=default&gadget=http:"+environment.node_cur+"/osapp/SKEApps/app.xml&debug=1&nocache=1&html_tag_context=script&url=http:"+environment.node_cur+"/osapp/SKEApps/javascripts/"+javascripts[0];
-	if(javascripts.length > 1)
-		src = environment.current+"/gadgets/concat?container=default&gadget=http:"+environment.node_cur+"/osapp/SKEApps/app.xml&debug=1&nocache=1&type=js&";
-	for(var i = 1 ; i < javascripts.length+1 ; i++){
-		src += i.toString()+"=http:"+environment.node_cur+"/osapp/SKEApps/javascripts/"+javascripts[i-1]+"&";
-	}
-	var script = document.createElement( 'script' );
-	script.src = src;
-	$("body").append( script );
-	switch(space_id){
-		case clients.weight_watchers:
-			functions.ww();
-		break;
 	}
 }
 
